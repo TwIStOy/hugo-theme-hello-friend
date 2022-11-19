7c77a3be59824a8fc64636b95144d192e62f2d18
Hawtian Wang <twistoy.wang@gmail.com>
2022-11-19 15:13:33 +0800

    fix

---
+++ assets/js/pre-mac.js
@@ -0,0 +1,14 @@
+/*
+ * const container = document?.querySelector(".container");
+ * const codeBlocks = container?.getElementsByTagName('pre')
+ * Array.from(codeBlocks).forEach(item => {
+ *   item.style.whiteSpace = 'pre-wrap'
+ *   // Add pre-mac element for Mac Style UI
+ *   const preMac = document.createElement('div')
+ *   preMac.classList.add('pre-mac')
+ *   preMac.innerHTML = '<span></span><span></span><span></span>'
+ *   item.parentElement.insertBefore(preMac, item)
+ * 
+ *   item.classList.add('line-numbers');
+ * })
+ */
